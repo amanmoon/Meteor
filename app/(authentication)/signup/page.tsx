6c55ae0202c7d6695956d10c9a4f209207d1c9cc
adrinospy <amanmoon096@gmail.com>
2024-12-22 09:02:57 +0530

    added Google OAuth

--- app/(authentication)/signup/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>This is a signup page</h1>
-  }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-export const metadata = {
-  title: 'Meteor',
-  description: 'Your AI powered video calling app',
-}
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
 
 export default function RootLayout({
   children,
@@ -10,7 +9,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <title>Meteor</title>
+      <meta property="og:title" content="Meteor" />
+      <meta name="description" content="Your anywhere videocalling sollution!" />
+      <meta property="og:image" content="http://example.com/image.jpg" />
+      <meta property="og:type" content="website" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="author" content="Your Name" />
+      <body><SessionProvider>{children}</SessionProvider></body>
     </html>
   )
 }
